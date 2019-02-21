1504a9f925e3224099fde11a5f40f7ebbae7ee8e
m3phistopheles <veroalucard@gmail.com>
2019-02-21 09:48:58 -0700

    adding first few tests

---
+++ cypress/integration/pokeApiSample_spec.js
@@ -0,0 +1,43 @@
+describe('Making test calls to pokeAPI endpoints ', function () {
+    var result
+
+    it('should validate the berry flavors header ', function() {
+        result = cy.request({
+            method: 'GET',
+            url: '/berry-flavor/5/'
+        })
+        result.its('headers')
+              .its('content-type')
+              .should('include', 'application/json')
+    })
+
+    it('should use baseUrl to call pokeAPI endpoint for charmander. This will log the json response into the debug console and verify that the response status is successful', function () {
+        cy.request({
+            method:'GET', 
+            url: '/pokemon/4'
+        }).then((response) => {
+            cy.log(response.body)
+            assert.equal(response.status, 200);
+            expect(response.body).to.not.be.null;
+        })
+    })
+
+    it('should call the bulbasaur stats as a cypress environment and verify the amount of moves available', function() {
+        result = cy.request({
+             method: 'GET',
+             url: Cypress.env('bulbasaurStats')
+         })
+        result.its('body.moves')
+            .should('have.length', 66);
+    })
+
+    it('should save information related to psyduck into a json file ', function() {
+        cy.request({
+            method: 'GET',
+            url: '/pokemon/54'
+        }).then((response) => {
+            cy.writeFile('/cypress/fixtures/psyduckStats.json', response.body)
+        })
+        cy.readFile('/cypress/fixtures/psyduckStats.json')
+    })
+})
---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,7 @@
+describe('My First Test', function () {
+  it('Does not do much!', function () {
+    cy.visit('www.google.com')
+    expect(true).to.equal(true)
+  })
+})
+
